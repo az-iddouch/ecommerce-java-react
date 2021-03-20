ccd89194dafdbea143525e5929904d150fabdeba
Abdelaziz Iddouch <abdelaziz.iddouch@gmail.com>
2021-03-20 12:27:40 +0100

    WIP on home page

--- frontend/src/CityCard.js
+++ frontend/src/CityCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Card.css';
+import './CityCard.css';
 
-function Card({ src, cityName, numberOfPostings }) {
+function CityCard({ src, cityName, numberOfPostings }) {
   return (
     <div className="city-card">
       <img src={src} alt={cityName} />
       <div className="city-card__infos">
         <h3>{cityName}</h3>
-        <h5>{numberOfPostings}</h5> Listings
+        <h5>{numberOfPostings}</h5> <span>Listings</span>
       </div>
     </div>
   );
 }
 
-export default Card;
+export default CityCard;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import './Home.css';
 import Banner from './Banner';
-import Card from './Card';
+import CityCard from './CityCard';
 
 function Home() {
   return (
     <div className="home">
       <Banner />
       <div className="home__section">
-        <Card
+        <CityCard
           cityName="Agadir"
           src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
           numberOfPostings={100}
         />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
+        <CityCard
+          cityName="Agadir"
+          src="https://media.istockphoto.com/photos/marina-in-agadir-city-at-sunset-morocco-picture-id845800272?k=6&m=845800272&s=612x612&w=0&h=iu9ocf9HZNufnz37ncjeKaMZ-b4CO_zRaYkcACJ75_I="
+          numberOfPostings={100}
+        />
       </div>
     </div>
   );
