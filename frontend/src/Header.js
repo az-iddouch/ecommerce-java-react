9f2e55403f170081398d2595ec21f3c7b10cb37a
Abdelaziz Iddouch <abdelaziz.iddouch@gmail.com>
2021-03-21 22:09:00 +0100

    WIP on home page

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Button } from '@material-ui/core';
+import Search from './Search';
 import './Header.css';
 
 function Header() {
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="header">
       <div className="header__logo">
         <p>VASH</p>
       </div>
       <div className="header__center">
-        <input type="text" />
-        <div className="header__search-btn">
+        <Button
+          variant="outline"
+          className="header__search-btn"
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          {showSearch ? 'Hide search' : 'Start your search'}
           <SearchIcon />
-        </div>
+        </Button>
+        <div className="header__search">{showSearch && <Search />}</div>
       </div>
       <div className="header__right">
         <p>Become a host</p>
