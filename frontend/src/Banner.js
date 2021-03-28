42840403c2f36baefce26a54529cec84fa54b015
Abdelaziz Iddouch <abdelaziz.iddouch@gmail.com>
2021-03-28 19:01:59 +0100

    finished search page

--- frontend/src/Banner.js
+++ frontend/src/Banner.js
@@ -1,9 +1,12 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router';
 
 import './Banner.css';
 
 function Banner() {
+  const history = useHistory();
+
   return (
     <div className="banner">
       <div className="banner__info">
@@ -11,7 +14,9 @@ function Banner() {
         <h5>
           Plane a different kind of getaway to uncover the hidden gems near you.
         </h5>
-        <Button variant="outlined">Explore Nearby</Button>
+        <Button variant="outlined" onClick={() => history.push('/search')}>
+          Explore Nearby
+        </Button>
       </div>
     </div>
   );
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -3,13 +3,16 @@ import SearchIcon from '@material-ui/icons/Search';
 import { Avatar, Button } from '@material-ui/core';
 import Search from './Search';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="header">
       <div className="header__logo">
-        <p>VASH</p>
+        <Link to="/">
+          <p>VASH</p>
+        </Link>
       </div>
       <div className="header__center">
         <Button
