17acec8baf8864f6eb2b6612107caa7b6db1d92c
Abdelaziz Iddouch <abdelaziz.iddouch@gmail.com>
2021-03-30 23:09:44 +0100

    WIP on listing page

--- frontend/src/pages/Listing.js
+++ frontend/src/pages/Listing.js
@@ -1,11 +1,28 @@
 import { LocationOn, Star } from '@material-ui/icons';
-import React from 'react';
+import CropOutlinedIcon from '@material-ui/icons/CropOutlined';
+import BathtubOutlinedIcon from '@material-ui/icons/BathtubOutlined';
+import HomeWorkOutlinedIcon from '@material-ui/icons/HomeWorkOutlined';
+import WeekendOutlinedIcon from '@material-ui/icons/WeekendOutlined';
+import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import React, { useState } from 'react';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/dist/photoswipe.css';
 import 'photoswipe/dist/default-skin/default-skin.css';
 import './Listing.css';
 
 function Listing() {
+  const [selectedDate, setSelectedDate] = useState(
+    new Date('2014-08-18T21:11:54')
+  );
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
   return (
     <div className="listing content">
       <h2>CHAMBRE ENSOLEILLÉE ET CONFORTABLE // PRIVATE ROOM</h2>
@@ -92,6 +109,59 @@ function Listing() {
           </Item>
         </div>
       </Gallery>
+      <div className="listing__content">
+        <div className="listing__content--left">
+          <h3>Details</h3>
+          {/* Surface / Number of rooms / Number of WC / Equipped */}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam odio
+            maxime ducimus distinctio! Totam, libero dolorum obcaecati tempore
+            eum autem esse corrupti voluptas cum cumque maiores temporibus
+            accusantium similique ea sed dolorem cupiditate dolores molestiae
+            nihil facilis. Porro at optio voluptas esse? Iusto quisquam
+          </p>
+          <ul>
+            <li>
+              <CropOutlinedIcon />
+              Surface: 50 m²
+            </li>
+            <li>
+              <HomeWorkOutlinedIcon />
+              Rooms: 3
+            </li>
+            <li>
+              <BathtubOutlinedIcon />
+              Bathrooms: 2
+            </li>
+            <li>
+              <WeekendOutlinedIcon />
+              Equipped
+            </li>
+          </ul>
+        </div>
+        <div className="listing__content--right">
+          <div className="listing__content--right-card">
+            <h2>
+              MAD120 <span>/night</span>
+            </h2>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Date picker inline"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
