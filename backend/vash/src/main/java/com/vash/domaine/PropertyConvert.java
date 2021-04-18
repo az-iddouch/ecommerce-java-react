b1e0bc312dc3c550f9c4db136c761344346069a1
jocker <mabrouk.ilias@gmail.com>
2021-04-18 23:14:59 +0000

    WIP reservation

--- backend/vash/src/main/java/com/vash/domaine/PropertyConvert.java
+++ backend/vash/src/main/java/com/vash/domaine/PropertyConvert.java
@@ -8,8 +8,7 @@ import org.springframework.util.ObjectUtils;
 import com.vash.entities.Property;
 
 public class PropertyConvert {
-
-	
+	/** reservation active **/
 	public static Property toBo(PropertyVo propertyVo) {
 		Property property = null;
 		if (!ObjectUtils.isEmpty(propertyVo)) {
@@ -31,12 +30,13 @@ public class PropertyConvert {
 			property.setTypeProperty(TypePropertyConverter.toBo(propertyVo.getTypeProperty()));
 			property.setUser(UserConverter.toBo(propertyVo.getUser()));
 			property.setCity(CityConverter.toBo(propertyVo.getCity()));
-			
+
 		}
 
 		return property;
 	}
-	
+
+	/** reservation active **/
 	public static PropertyVo toVo(Property property) {
 		PropertyVo propertyVo = null;
 		if (!ObjectUtils.isEmpty(property)) {
@@ -58,25 +58,25 @@ public class PropertyConvert {
 			propertyVo.setTypeProperty(TypePropertyConverter.toVo(property.getTypeProperty()));
 			propertyVo.setUser(UserConverter.toVo(property.getUser()));
 			propertyVo.setCity(CityConverter.toVo(property.getCity()));
-			
+
 		}
 
 		return propertyVo;
 	}
-	
+
+	/** reservation active **/
 	public static List<Property> toListBo(List<PropertyVo> propertyVos) {
 		List<Property> propertys = new ArrayList<Property>();
 		if (!ObjectUtils.isEmpty(propertyVos)) {
 			for (PropertyVo propertyVo : propertyVos) {
 				propertys.add(toBo(propertyVo));
 			}
-			
+
 		}
 
 		return propertys;
 	}
-	
-	
+
 	public static List<PropertyVo> toListVo(List<Property> propertys) {
 		List<PropertyVo> propertyVos = new ArrayList<PropertyVo>();
 		if (!ObjectUtils.isEmpty(propertys)) {
@@ -87,4 +87,87 @@ public class PropertyConvert {
 
 		return propertyVos;
 	}
+
+	// ---------
+	/** reservation desactive **/
+	public static Property toBo2(PropertyVo propertyVo) {
+		Property property = null;
+		if (!ObjectUtils.isEmpty(propertyVo)) {
+			property = new Property();
+			property.setId(propertyVo.getId());
+			property.setDescription(propertyVo.getDescription());
+			property.setEquiped(propertyVo.getEquiped());
+			property.setNumberMaxPersons(propertyVo.getNumberMaxPersons());
+			property.setNumberRoom(propertyVo.getNumberRoom());
+			property.setNumberWC(propertyVo.getNumberWC());
+			property.setPrice(propertyVo.getPrice());
+			property.setSurface(propertyVo.getSurface());
+			property.setVisible(propertyVo.getVisible());
+			property.setAddress(AddressConverter.toBo(propertyVo.getAddress()));
+			property.setImages(ImageConverter.toListBo(propertyVo.getImages()));
+			// property.setReservations(ReservationConvert.toListBo(propertyVo.getReservations()));
+			property.setReviews(ReviewConverter.toListBo(propertyVo.getReviews()));
+			property.setTags(TagsConverter.toListBo(propertyVo.getTags()));
+			property.setTypeProperty(TypePropertyConverter.toBo(propertyVo.getTypeProperty()));
+			property.setUser(UserConverter.toBo(propertyVo.getUser()));
+			property.setCity(CityConverter.toBo(propertyVo.getCity()));
+
+		}
+
+		return property;
+	}
+
+	/** reservation desactive **/
+	public static PropertyVo toVo2(Property property) {
+		PropertyVo propertyVo = null;
+		if (!ObjectUtils.isEmpty(property)) {
+			propertyVo = new PropertyVo();
+			propertyVo.setId(property.getId());
+			propertyVo.setDescription(property.getDescription());
+			propertyVo.setEquiped(property.getEquiped());
+			propertyVo.setNumberMaxPersons(property.getNumberMaxPersons());
+			propertyVo.setNumberRoom(property.getNumberRoom());
+			propertyVo.setNumberWC(property.getNumberWC());
+			propertyVo.setPrice(property.getPrice());
+			propertyVo.setSurface(property.getSurface());
+			propertyVo.setVisible(property.getVisible());
+			propertyVo.setAddress(AddressConverter.toVo(property.getAddress()));
+			propertyVo.setImages(ImageConverter.toListVo(property.getImages()));
+			// propertyVo.setReservations(ReservationConvert.toListVo(property.getReservations()));
+			propertyVo.setReviews(ReviewConverter.toListVo(property.getReviews()));
+			propertyVo.setTags(TagsConverter.toListVo(property.getTags()));
+			propertyVo.setTypeProperty(TypePropertyConverter.toVo(property.getTypeProperty()));
+			propertyVo.setUser(UserConverter.toVo(property.getUser()));
+			propertyVo.setCity(CityConverter.toVo(property.getCity()));
+
+		}
+
+		return propertyVo;
+	}
+
+	/** reservation desactive **/
+	public static List<Property> toListBo2(List<PropertyVo> propertyVos) {
+		List<Property> propertys = new ArrayList<Property>();
+		if (!ObjectUtils.isEmpty(propertyVos)) {
+			for (PropertyVo propertyVo : propertyVos) {
+				propertys.add(toBo2(propertyVo));
+			}
+
+		}
+
+		return propertys;
+	}
+
+	/** reservation desactive **/
+	public static List<PropertyVo> toListVo2(List<Property> propertys) {
+		List<PropertyVo> propertyVos = new ArrayList<PropertyVo>();
+		if (!ObjectUtils.isEmpty(propertys)) {
+			for (Property property : propertys) {
+				propertyVos.add(toVo2(property));
+			}
+		}
+
+		return propertyVos;
+	}
+
 }
